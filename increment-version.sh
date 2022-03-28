ddb3a9d501b80e99d53835a592fbba3300363538
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-03-27 22:56:31 -0400

    creating skel

--- increment-version.sh
+++ increment-version.sh
@@ -4,3 +4,5 @@
 cat VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}' > VERSION.tmp && mv VERSION.tmp VERSION
 version=`cat VERSION`
 
+sed -e "s/[0-9].[0-9].[0-9]/${version}/" nginx-deployment.yaml > nginx-deployment.yaml.tmp && mv nginx-deployment.yaml.tmp nginx-deployment.yaml
+
--- update-manifest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-
-version=`cat VERSION`
-
-sed -e "s/[0-9].[0-9].[0-9]/${version}/" nginx-deployment.yaml > nginx-deployment.yaml.tmp && mv nginx-deployment.yaml.tmp nginx-deployment.yaml
-
