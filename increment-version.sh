2469aecf863128306729db99c0b1b638a076d2b0
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-01 19:55:26 -0400

    creating skel

--- increment-version.sh
+++ increment-version.sh
@@ -6,3 +6,5 @@ version=`cat nginx/VERSION`
 
 sed -e "s/[0-9].[0-9].[0-9]/${version}/" nginx/deployment.yaml > nginx/deployment.yaml.tmp && mv nginx/deployment.yaml.tmp nginx/deployment.yaml
 
+
+sed -e "s/[0-9].[0-9].[0-9]/${version}/" nginx.yaml > nginx.yaml.tmp && mv nginx.yaml.tmp nginx.yaml
