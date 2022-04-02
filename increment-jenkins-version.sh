d274a5bcb0bd605d8a1110994440f4991271fde3
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-02 02:56:24 -0400

    creating skel

--- increment-jenkins-version.sh
+++ increment-jenkins-version.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
 
-cat jenkins/VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}' > jenkins/VERSION.tmp && mv jenkins/VERSION.tmp jenkins/VERSION
-version=`cat jenkins/VERSION`
+
+sed -re "s/targetRevision: [0-9]+\.[0-9]+\.[0-9]+-*([0-9]*)/targetRevision: `cat jenkins/VERSION`/" jenkins.yaml > jenkins.yaml.tmp && mv jenkins.yaml.tmp jenkins.yaml
 
 
-sed -e "s/[0-9].[0-9].[0-9]/${version}/" jenkins.yaml > jenkins.yaml.tmp && mv jenkins.yaml.tmp jenkins.yaml
