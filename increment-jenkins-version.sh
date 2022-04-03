3676f13aea691a8574dde8aa75230b23d0dc3fcd
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-02 22:35:00 -0400

    creating skel

--- increment-jenkins-version.sh
+++ increment-jenkins-version.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #
 
+curl -s https://raw.githubusercontent.com/robinmordasiewicz/jenkins-container/main/VERSION > jenkins/VERSION
+
 sed -re "s/targetRevision: [0-9]+\.[0-9]+\.[0-9]+-*([0-9]*)/targetRevision: `cat jenkins/VERSION`/" jenkins.yaml > jenkins.yaml.tmp && mv jenkins.yaml.tmp jenkins.yaml
 
