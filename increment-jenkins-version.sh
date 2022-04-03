58e411e19baa5025d5f185d3ca0389f9bae2a2cd
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-03 01:13:18 -0400

    creating skel

--- increment-jenkins-version.sh
+++ increment-jenkins-version.sh
@@ -5,4 +5,4 @@ curl -s https://raw.githubusercontent.com/robinmordasiewicz/helm-charts/main/VER
 
 HELMVERSION=`cat jenkins/VERSION`
 
-cat jenkins.yaml | sed -e "s/targetRevision:.*/targetRevision: ${HELMVERSION}\"/"  > jenkins.yaml.tmp && mv jenkins.yaml.tmp jenkins.yaml
+cat jenkins.yaml | sed -e "s/targetRevision:.*/targetRevision: \"${HELMVERSION}\"/"  > jenkins.yaml.tmp && mv jenkins.yaml.tmp jenkins.yaml
