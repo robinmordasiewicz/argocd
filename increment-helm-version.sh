7e7eda5a3777d6c98c6a713559dac0f9775366e2
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-07 15:37:55 -0400

    New pipeline test

--- increment-helm-version.sh
+++ increment-helm-version.sh
@@ -2,11 +2,11 @@
 #
 
 #curl -s https://raw.githubusercontent.com/robinmordasiewicz/helm-charts/main/VERSION.helmchart > jenkins/VERSION
-if [ -d tmp ]; && rm -rf tmp
+[ -d tmp ] && rm -rf tmp
 mkdir tmp
 git clone https://github.com/robinmordasiewicz/helm-charts.git tmp/
 cp tmp/VERSION.helmchart jenkins/VERSION
-if [ -d tmp ]; && rm -rf tmp
+[ -d tmp ] && rm -rf tmp
 
 HELMVERSION=`cat jenkins/VERSION`
 
