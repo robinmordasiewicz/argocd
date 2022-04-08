fe5213f61c1faa5ecd1961a1855015449285b87d
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-07 21:05:59 -0400

    New pipeline test

--- increment-nginx-version.sh
+++ increment-nginx-version.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 #
 
+
+HELMVERSION=`cat jenkins/VERSION`
+
+cat jenkins.yaml | sed -e "s/targetRevision:.*/targetRevision: \"${HELMVERSION}\"/"  > jenkins.yaml.tmp && mv jenkins.yaml.tmp jenkins.yaml
+
+[ -d tmp ] && rm -rf tmp
+mkdir tmp
+git clone https://github.com/robinmordasiewicz/nginx.git tmp/
+cp tmp/VERSION nginx/VERSION
+[ -d tmp ] && rm -rf tmp
+
 cat nginx/VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}' > nginx/VERSION.tmp && mv nginx/VERSION.tmp nginx/VERSION
 version=`cat nginx/VERSION`
 
