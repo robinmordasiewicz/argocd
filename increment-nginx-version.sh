1b17b81a80d61dc9f60a606040a04651c0a1ad1c
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-10 17:09:33 -0400

    arranging triggers

--- increment-nginx-version.sh
+++ increment-nginx-version.sh
@@ -11,7 +11,6 @@ cp tmp/VERSION nginx/VERSION
 # cat nginx/VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}' > nginx/VERSION.tmp && mv nginx/VERSION.tmp nginx/VERSION
 version=`cat nginx/VERSION`
 
-sed -e "s/[0-9].[0-9].[0-9]/${version}/" nginx/deployment.yaml > nginx/deployment.yaml.tmp && mv nginx/deployment.yaml.tmp nginx/deployment.yaml
+sed -e "s/image: robinhoodis\/nginx:.*/image: robinhoodis\/nginx:${version}/" nginx/deployment.yaml > nginx/deployment.yaml.tmp && mv nginx/deployment.yaml.tmp nginx/deployment.yaml
 
-
-sed -e "s/[0-9].[0-9].[0-9]/${version}/" nginx.yaml > nginx.yaml.tmp && mv nginx.yaml.tmp nginx.yaml
+sed -e "s/targetRevision:.*/targetRevision:${version}/" nginx.yaml > nginx.yaml.tmp && mv nginx.yaml.tmp nginx.yaml
