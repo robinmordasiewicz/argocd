0d68364ed9ddf9602b3c123a06d05963cd1dc3a5
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-10 17:23:40 -0400

    arranging triggers

--- increment-nginx-version.sh
+++ increment-nginx-version.sh
@@ -13,4 +13,4 @@ version=`cat nginx/VERSION`
 
 sed -e "s/image: robinhoodis\/nginx:.*/image: robinhoodis\/nginx:${version}/" nginx/deployment.yaml > nginx/deployment.yaml.tmp && mv nginx/deployment.yaml.tmp nginx/deployment.yaml
 
-sed -e "s/targetRevision:.*/targetRevision:${version}/" nginx.yaml > nginx.yaml.tmp && mv nginx.yaml.tmp nginx.yaml
+sed -e "s/targetRevision:.*/targetRevision: ${version}/" nginx.yaml > nginx.yaml.tmp && mv nginx.yaml.tmp nginx.yaml
